ae82c057733fa64b4d6095a302bac01da64ac664
icarusJr <42274831+icarusJr@users.noreply.github.com>
2019-01-19 23:17:32 -0500

    Update conf.js

--- CyberBank/Configuration/conf.js
+++ CyberBank/Configuration/conf.js
@@ -100,21 +100,21 @@ exports.config = {
 		});
 
 		
-// 		var AllureReporter = require('jasmine-allure-reporter');
-// jasmine.getEnv().addReporter(new AllureReporter({
-//   resultsDir: 'allure-results'
-// }));
+		var AllureReporter = require('jasmine-allure-reporter');
+jasmine.getEnv().addReporter(new AllureReporter({
+  resultsDir: 'allure-results'
+}));
 		
-// 		var AllureReporter = require('jasmine-allure-reporter');
-//     jasmine.getEnv().addReporter(new AllureReporter());
-//     jasmine.getEnv().afterEach(function(done){
-//       browser.takeScreenshot().then(function (png) {
-//         allure.createAttachment('Screenshot', function () {
-//           return new Buffer(png, 'base64')
-//         }, 'image/png')();
-//         done();
-//       })
-//     });
+		var AllureReporter = require('jasmine-allure-reporter');
+    jasmine.getEnv().addReporter(new AllureReporter());
+    jasmine.getEnv().afterEach(function(done){
+      browser.takeScreenshot().then(function (png) {
+        allure.createAttachment('Screenshot', function () {
+          return new Buffer(png, 'base64')
+        }, 'image/png')();
+        done();
+      })
+    });
 
 	},
 
